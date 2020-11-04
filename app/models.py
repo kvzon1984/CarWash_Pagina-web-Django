c38d27a00070314b586e38232ec61d1aa25d1df3
Kvzon <ivega.josue@gmail.com>
2020-11-04 04:20:40 -0300

    modificacion models

--- app/models.py
+++ app/models.py
@@ -11,14 +11,11 @@ class Services(models.Model):
     def __str__(self):
         return self.name
 
-<<<<<<< HEAD
 #python .\manage.py makemigrations
 
 
 
-=======
 class Insumo (models.Model):
->>>>>>> 7a41d43ae65f9255b615a093fb968a560a2c6012
 
     name = models.CharField(max_length= 120)
     price = models.IntegerField()
@@ -31,19 +28,6 @@ class Insumo (models.Model):
 
 
 class Slider(models.Model):
-<<<<<<< HEAD
-    name = models.IntegerField() 
-    image = models.ImageField(upload_to="sliders", null=True)
-
-    def __init__(self):
-        return self.name
-
-class Gallery(models.Model):
-    name = models.IntegerField() 
-    image = models.ImageField(upload_to="gallery", null=True)
-
-    def __init__(self):
-=======
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to="sliders", null=True)
 
@@ -56,5 +40,4 @@ class Gallery(models.Model):
     image = models.ImageField(upload_to="gallery", null=True)
 
     def __str__(self):
->>>>>>> b63764345592fb212ac82a954a05ce249fe0792f
         return self.name
