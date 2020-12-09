a15ce9a11d21fffd2fe40860c8d43da0213cbcba
carlos vidal <51520836+KrotthY@users.noreply.github.com>
2020-12-09 03:26:59 -0300

    Correcion en la base de datos

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 from django.http import Http404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, permission_required
-from rest_framework import  serializers, viewsets
+from rest_framework import   viewsets
 from .serializers import InsumoSerializer, ContactoSerializer
 
 # Create your views here.
